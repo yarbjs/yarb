adcaa54ac935ed83929c4fcddbb04e27178d3a3d
Nick Jones <hello@nickmjones.me>
2019-07-29 23:00:32 -0400

    more additions

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,12 +4,13 @@ class Home extends Component {
   render() {
     return(
       <div>
-        <h1>Nick Jones</h1>
-        <h2>Nick Jones</h2>
-        <h3>Nick Jones</h3>
         <h4>Nick Jones</h4>
-        <h5>Nick Jones</h5>
         <p>A designer and developer in New York City.</p>
+
+        <div className="row">
+          <div className="row__element">Here is some content</div>
+          <div className="row__element">Here is some content</div>
+        </div>
       </div>
     )
   }
