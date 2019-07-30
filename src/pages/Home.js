aa595424df7cee3124859b6528cbf39a0a7ad42d
Nick Jones <hello@nickmjones.me>
2019-07-29 23:10:46 -0400

    Adds some new modules/babel plugins

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,11 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Home extends Component {
+class Home extends React.Component {
   render() {
     return(
       <div>
         <h4>Nick Jones</h4>
         <p>A designer and developer in New York City.</p>
-
         <div className="row">
           <div className="row__element">Here is some content</div>
           <div className="row__element">Here is some content</div>
