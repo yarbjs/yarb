5a2850eb9c0e0ae8d7cbc9c2a0b1d706895de1af
Nick Jones <hello@nickmjones.me>
2019-08-13 23:17:09 -0400

    - Tidy up gitignore - Removed an unused dir

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "Starting..."
+
+# Remove /dist if it exists
+echo "Removing old builds..."
+rm -rfv ./dist
+mkdir ./dist
+
+echo "Starting Parcel..."
+parcel build public/index.html
+
+echo "Build done. Starting deployment..."
+cd ./dist
+echo "Deploying..."
+now
+echo "Done."
--- src/views/DefaultLayout.js
+++ src/views/DefaultLayout.js
@@ -2,11 +2,6 @@ import React from 'react'
 import Routes from '../Routes'
 
 class DefaultLayout extends React.Component {
-
-  state = {
-    layoutCreatedOn: Date(),
-  };
-
   render() {
     return(
       <div className="wrapper">
